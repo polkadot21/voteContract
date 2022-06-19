38afa216e663e0b9cb77caedb714d98d95ed5d81
evgenysaurov <saurov.ev@gmail.com>
2022-06-19 11:56:18 +0200

    send funds to the contract's address first; add function to wire owner's funds

--- contracts/voteContract.sol
+++ contracts/voteContract.sol
@@ -10,9 +10,9 @@ pragma experimental ABIEncoderV2; // This will generate a cautionary warning.
 contract VoteApp  {
     address payable public owner; // owner of the contract
     address payable public winner; // winner
+    address payable public contractAddress; //address to collect funds
     uint256 public contributionAmount; // contribution required to vote
     uint256 public numberOfDays; // duration of the voting
-    uint256 public numberOfSeconds; // duration in seconds
     uint256 public raisedAmount; // the sum of all contributions
     uint256 public deadline; //the end of the voting
 
@@ -23,8 +23,8 @@ contract VoteApp  {
 
     struct Candidate {
         address payable candidate; // candidate
-        uint256 isRegistered; // candidate is registered true=1, false=0
-        uint256 amountCollected; // the amount of funds collected
+        // uint256 isRegistered; // candidate is registered true=1, false=0
+        uint256 numberOfVotes; // the number of votes collected
     }
 
     mapping(address => Contributor) public contributors;
@@ -32,9 +32,9 @@ contract VoteApp  {
 
     constructor() {
         owner = msg.sender;
-        numberOfSeconds = numberOfDays * 1 days;
-        deadline = block.timestamp + numberOfSeconds;
+        deadline = block.timestamp + numberOfDays * 1 days;
         contributionAmount = 10000000000000000 wei;
+        contractAddress = payable(address(this));
     }
 
     // Array with candidates
@@ -44,16 +44,16 @@ contract VoteApp  {
     enum WorkflowStatus {
         CandidateRegistration,
         VotingSessionStarted,
-        VotingSessionEnded,
-        RewardWiring
+        VotingSessionEnded
     }
 
     // Events
     WorkflowStatus currentStatus = WorkflowStatus.CandidateRegistration;
 
     event VotingSessionStarted();
-    event Contributed (address contibutor, address candidate);
-    event FundsWired(address winner, address owner);
+    //event Contributed (address contibutor, address candidate);
+    //event FundsWired(address winner, address owner);
+    event VotingSessionEnded();
     event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
 
 
@@ -88,67 +88,75 @@ contract VoteApp  {
         _;
     }
 
-    modifier candidateExists(address _address) {
-        require(
-            candidates[_address].isRegistered == 1,
-            "This candidate does not exist"
-        );
-        _;
-    }
-
     modifier hasStatus(WorkflowStatus status) {
         require(keccak256(abi.encodePacked(currentStatus)) == keccak256(abi.encodePacked(status)), "You cannot do this, invalid workflow status");
         _;
     }
 
+    modifier hasEnoughFunds(address _adress) {
+        require(msg.sender.balance >= contributionAmount,
+            "0.01 eth is required to contribute."
+        );
+        _;
+    }
+
 
     // create vote
-    function startVoting(address[] memory _candidates) public onlyOwner hasStatus(WorkflowStatus.CandidateRegistration) {
+    function createVoting(address[] memory _candidates) public onlyOwner hasStatus(WorkflowStatus.CandidateRegistration) {
         emit VotingSessionStarted();
         emit WorkflowStatusChange(WorkflowStatus.CandidateRegistration, WorkflowStatus.VotingSessionStarted);
-
         currentStatus = WorkflowStatus.VotingSessionStarted;
+
         for (uint i=0; i < _candidates.length; i++) {
-            allCandidates.push(Candidate(payable(_candidates[i]), 1, 0));
+            allCandidates.push(Candidate(payable(_candidates[i]), 0));
         }
     }
 
     //Contibute
-    function contribute(address _candidate) public payable isNotExpired notVoted(msg.sender) hasStatus(WorkflowStatus.VotingSessionStarted) {
+    function contribute(address _candidate) public payable isNotExpired notVoted(msg.sender) hasEnoughFunds(msg.sender) hasStatus(WorkflowStatus.VotingSessionStarted) {
 
         require(
-            msg.value >= contributionAmount,
-            "0.01 eth is required to contribute."
+            msg.value == contributionAmount,
+            "You can only contribute with 0.01 eth"
         );
 
         contributors[msg.sender].hasVoted = 1; // voted
-        raisedAmount += contributionAmount; // increase the entire amount raised
-        candidates[_candidate].amountCollected += contributionAmount; //increase the candidate's amount
-        emit Contributed(msg.sender, _candidate);
+        raisedAmount += msg.value; // increase the entire amount raised
+        candidates[_candidate].numberOfVotes += 1; //increase the candidate's amount
+        contractAddress.transfer(msg.value);
+        //emit Contributed(msg.sender, _candidate);
     }
+
 
     // stop vote
     function endVoting() public payable isExpired hasStatus(WorkflowStatus.VotingSessionStarted) {
+        emit VotingSessionEnded();
+        emit WorkflowStatusChange(WorkflowStatus.VotingSessionStarted, WorkflowStatus.VotingSessionEnded);
         currentStatus = WorkflowStatus.VotingSessionEnded;
+
 
         //find winner
         uint winningAmount = 0;
         for (uint i = 0; i < allCandidates.length; i++) {
-            if (allCandidates[i].amountCollected > winningAmount) {
-                winningAmount = allCandidates[i].amountCollected;
+            if (allCandidates[i].numberOfVotes > winningAmount) {
+                winningAmount = allCandidates[i].numberOfVotes;
                 winner = allCandidates[i].candidate;
             }
+
+            //send 90% of the amount rised to the winner wallet
+            uint256 winnerPrize = raisedAmount * 9 / 10 ;
+            winner.transfer(winnerPrize);
         }
+    }
 
-        //send 90% of the amount raised to the winner wallet
-        uint256 winnerPrize = raisedAmount * 9 / 10 ;
-        winner.transfer(winnerPrize);
 
-        //send 10% of the amount raised to the admin wallet
+    function wireFundsToOwner() public onlyOwner hasStatus(WorkflowStatus.VotingSessionEnded) {
+
+        //send 10% of the amount rised to the admin wallet
         uint256 adminPrize = raisedAmount / 10;
         owner.transfer(adminPrize);
 
-        emit FundsWired(winner, owner);
-        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.RewardWiring);
+        //emit FundsWired(winner, owner);
+        //emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.RewardWiring);
     }
 }
